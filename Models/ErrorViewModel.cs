76ac47e5f30bbc241ac5653000f2d3f8db9dd71f
Mudassar Rafiq <102991427+meharmudassar@users.noreply.github.com>
2022-06-29 10:27:51 +0500

    commit
    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mudasser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
